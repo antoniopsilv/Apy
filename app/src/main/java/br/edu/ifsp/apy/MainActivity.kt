77a2ab43e09a9316b227a0511b7b0fba8f6ffc04
antoniopsilv <antoniop.silv@gmail.com>
2025-06-09 22:57:59 -0300

    Implementation Gallery check permission

--- app/src/main/java/br/edu/ifsp/apy/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/apy/MainActivity.kt
@@ -1,14 +1,38 @@
 package br.edu.ifsp.apy
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.apy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+
+    private val activityMainActivity: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    companion object {
+        private val GALERY_PERMISSION = Manifest.permission.READ_EXTERNAL_STORAGE
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activityMainActivity.root)
+    }
+
+    private fun galeryPermissionCheck() {
+        val acceptGaleryPermission = checkPermission(GALERY_PERMISSION)
+    }
+
+    private fun checkPermission(permission: String) {
+        ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
     }
 }
