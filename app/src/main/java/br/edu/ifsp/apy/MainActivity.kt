2853c7b1db5574b84acd76952a8befeca416d502
antoniopsilv <antoniop.silv@gmail.com>
2025-06-15 12:05:11 -0300

    Implementation of opening images from the gallery

--- app/src/main/java/br/edu/ifsp/apy/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/apy/MainActivity.kt
@@ -1,9 +1,19 @@
 package br.edu.ifsp.apy
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Audio.Media
+import android.provider.Settings
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.content.PackageManagerCompat
@@ -13,26 +23,95 @@ import br.edu.ifsp.apy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
-
     private val activityMainActivity: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    // Teste 
+    private lateinit var dialog: AlertDialog
 
     companion object {
         private val GALERY_PERMISSION = Manifest.permission.READ_EXTERNAL_STORAGE
     }
 
+    private val requestGalery =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { permission ->
+            if(permission){
+                resultGallery.launch(
+                    Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    )
+                )
+            } else {
+                showDialogPermission()
+            }
+
+        }
+
+    private val resultGallery =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val bitmap: Bitmap =
+                if (Build.VERSION.SDK_INT < 28) {
+                    MediaStore.Images.Media.getBitmap(
+                    baseContext.contentResolver,
+                    result.data?.data
+                )
+                } else {
+                    val source = ImageDecoder.createSource(
+                    this.contentResolver,
+                    result.data?.data!!
+                )
+                ImageDecoder.decodeBitmap(source)
+            }
+            activityMainActivity.imageView.setImageBitmap(bitmap)
+        }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainActivity.root)
+
+        activityMainActivity.button.setOnClickListener{ galeryPermissionCheck() }
     }
 
     private fun galeryPermissionCheck() {
         val acceptGaleryPermission = checkPermission(GALERY_PERMISSION)
+
+        when {
+            acceptGaleryPermission -> {
+                resultGallery.launch(
+                    Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    )
+                )
+            }
+            shouldShowRequestPermissionRationale(GALERY_PERMISSION) -> showDialogPermission()
+            else -> requestGalery.launch(GALERY_PERMISSION)
+        }
     }
 
-    private fun checkPermission(permission: String) {
-        ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+    private fun showDialogPermission() {
+        val builder = AlertDialog.Builder(this)
+            .setTitle("Atenção")
+            .setMessage("É necessário o acesso a galeria do dispositivo, deseja permitir agora ?")
+            .setNegativeButton("Não") { _, _ ->
+                dialog.dismiss()
+            }
+            .setPositiveButton("Sim") { _, _ ->
+                val intent = Intent(
+                    Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.fromParts("package",packageName,null)
+                )
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                dialog.dismiss()
+            }
+
+        dialog = builder.create()
+        dialog.show()
+
     }
+
+    private fun checkPermission(permission: String) =
+        ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
 }
