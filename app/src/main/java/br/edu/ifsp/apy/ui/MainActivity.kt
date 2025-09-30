98e847d4ffc8f572fb2bab4170c948323cb0b3b4
antoniopsilv <antoniop.silv@gmail.com>
2025-09-30 02:35:37 -0300

    Refactor layout to Compose; improve graphical interface

--- app/src/main/java/br/edu/ifsp/apy/ui/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/apy/ui/MainActivity.kt
@@ -1,117 +1,99 @@
-package br.edu.ifsp.apy.ui
+    package br.edu.ifsp.apy.ui
 
-import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.ImageDecoder
-import android.net.Uri
-import android.os.Build
-import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
-import android.view.View
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatActivity.RESULT_OK
-import br.edu.ifsp.apy.classification.ImageClassification
-import br.edu.ifsp.apy.databinding.ActivityMainBinding
-import com.yalantis.ucrop.UCrop
-import org.tensorflow.lite.task.vision.classifier.Classifications
-import java.text.NumberFormat
+    import android.graphics.Bitmap
+    import android.graphics.ImageDecoder
+    import android.net.Uri
+    import android.os.Build
+    import android.os.Bundle
+    import android.provider.MediaStore
+    import android.widget.Toast
+    import androidx.activity.result.contract.ActivityResultContracts
+    import androidx.appcompat.app.AppCompatActivity
+    import br.edu.ifsp.apy.classification.ImageClassification
+    import br.edu.ifsp.apy.databinding.ActivityMainBinding
+    import org.tensorflow.lite.task.vision.classifier.Classifications
+    import java.text.NumberFormat
 
-class MainActivity : AppCompatActivity() {
+    class `123_MainActivity_old` : AppCompatActivity() {
 
-    private val binding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
+        private val binding: ActivityMainBinding by lazy {
+            ActivityMainBinding.inflate(layoutInflater)
+        }
 
-    private var currentImageUri: Uri? = null
+        private var currentImageUri: Uri? = null
 
-    private lateinit var imageClassification: ImageClassification
-    
-//    private lateinit var classifier: SkinCancerClassifier
+        private lateinit var imageClassification: ImageClassification
 
-    private val getImageFromGallery =
-        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
-            if (uri != null) {
-                try {
-                    val bitmap: Bitmap = loadBitmapFromUri(uri)
-                    binding.imageView.setImageBitmap(bitmap)
-
-
-
-                    // Continuar com o UCrop
-//                    UCrop.of(uri, Uri.fromFile(cacheDir.resolve("${System.currentTimeMillis()}.jpg")))
-//                        .withAspectRatio(16f, 9f)
-//                        .withMaxResultSize(2000, 2000)
-//                        .start(this)
-
-//                    val resultUri = UCrop.getOutput(Uri)
-                    currentImageUri= uri
-
-//                    binding.resultText.text = "" //resultText
-
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    Toast.makeText(this, "Erro ao processar imagem", Toast.LENGTH_SHORT).show()
+        private val getImageFromGallery =
+            registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+                if (uri != null) {
+                    try {
+                        val bitmap: Bitmap = loadBitmapFromUri(uri)
+                        binding.imageView.setImageBitmap(bitmap)
+    //                    UCrop.of(uri, Uri.fromFile(cacheDir.resolve("${System.currentTimeMillis()}.jpg")))
+    //                        .withAspectRatio(16f, 9f)
+    //                        .withMaxResultSize(2000, 2000)
+    //                        .start(this)
+    //                    val resultUri = UCrop.getOutput(Uri)
+                        currentImageUri= uri
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        Toast.makeText(this, "Erro ao processar imagem", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(this, "Nenhuma imagem selecionada", Toast.LENGTH_SHORT).show()
                 }
-            } else {
-                Toast.makeText(this, "Nenhuma imagem selecionada", Toast.LENGTH_SHORT).show()
+            }
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(binding.root)
+
+            binding.buttonSelectImage.setOnClickListener {
+                getImageFromGallery.launch("image/*")
+            }
+
+            binding.buttonAnalyzeImage.setOnClickListener {
+                currentImageUri?.let {
+                    analyzeImage(it)
+                }
             }
         }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        private fun analyzeImage(it: Uri) {
 
-//        classifier = SkinCancerClassifier(this)
+            imageClassification = ImageClassification (
+                context = this,
+                classifierListener = object : ImageClassification.ClassifierListener {
 
-        binding.buttonSelectImage.setOnClickListener {
-            getImageFromGallery.launch("image/*")
-        }
+                        override fun onError(error: String) {
+                            runOnUiThread {
+                                Toast.makeText(this@`123_MainActivity_old`, "Erro(99) na Classificação ", Toast.LENGTH_SHORT).show()
+                            }
+                        }
 
-        binding.buttonAnalyzeImage.setOnClickListener {
-            currentImageUri?.let {
-                analyzeImage(it)
-            }
-        }
-    }
-
-    private fun analyzeImage(it: Uri) {
-
-        imageClassification = ImageClassification (
-            context = this,
-            classifierListener = object : ImageClassification.ClassifierListener {
-
-                    override fun onError(error: String) {
+                        override fun onResults(results: List<Classifications>?) {
                         runOnUiThread {
-//                            showToast(this@MainActivity, error)
-                            Toast.makeText(this@MainActivity, "Erro(99) na Classificação ", Toast.LENGTH_SHORT).show()
+                            val resultText = results?.joinToString("\n") {
+                                it.categories[0].label + ": " +
+                                        NumberFormat.getPercentInstance()
+                                    .format(it.categories[0].score).trim()
+                            }
+                            binding.resultText.text = resultText
                         }
-                    }
-
-                    override fun onResults(results: List<Classifications>?) {
-                    runOnUiThread {
-                        val resultText = results?.joinToString("\n") {
-                            it.categories[0].label + ": " +
-                                    NumberFormat.getPercentInstance()
-                                .format(it.categories[0].score).trim()
-                        }
-                        binding.resultText.text = resultText
                     }
                 }
+            )
+            imageClassification.classifyStationImage(it)
+        }
+    
+        private fun loadBitmapFromUri(uri: Uri): Bitmap {
+            return if (Build.VERSION.SDK_INT < 28) {
+                MediaStore.Images.Media.getBitmap(contentResolver, uri)
+            } else {
+                val source = ImageDecoder.createSource(contentResolver, uri)
+                ImageDecoder.decodeBitmap(source)
             }
-        )
-        imageClassification.classifyStationImage(it)
-    }
-
-    private fun loadBitmapFromUri(uri: Uri): Bitmap {
-        return if (Build.VERSION.SDK_INT < 28) {
-            MediaStore.Images.Media.getBitmap(contentResolver, uri)
-        } else {
-            val source = ImageDecoder.createSource(contentResolver, uri)
-            ImageDecoder.decodeBitmap(source)
         }
     }
-}
 
